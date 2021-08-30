3155fd4d0e69157d8a97838f3a0720c672cf87bf
Anhlinh23 <84840271+Anhlinh23@users.noreply.github.com>
2021-08-30 21:30:48 +0700

    linh

---
+++ js/index.js
@@ -0,0 +1,43 @@
+// console.log("hello")
+// let myLover = 12
+// console.log(myLover)
+
+// let myAge = 24
+
+// console.log(myAge)
+// const BIRTH = "23/15/111"
+
+// let yourAge = prompt("Nam bao nhiu")
+// console.log("tuoi la", yourAge)
+// // cách đặt tên biến
+// // * có thể có số và chữ nhưng không thể bắt đàu bằng số
+// // * không được trùng với từ khóa
+// // * tên biến phải có nghĩa bằng ta 
+// // * 
+// // ket: biến: camelCase
+// // const: hằng: snake_case
+// let message = 'Hello!'; // define the variable and assign the value
+
+// alert(message); // Hello!
+// let x = 5;
+// let x = 10;
+//BÀI 3
+// let Code = "Coding is great";
+// console.log(Code);
+// let studentCount = 0;
+// console.log(studentCount);
+//BÀI 5 6 
+// alert ("you look beatyfull a day");
+// let yourName = prompt("Hi there, you name please ?");
+// console.log("name",yourName);
+// alert ("Hi," + yourName);
+// //BÀI 7
+// let firstName = prompt("Enter your first name");
+// console.log("name",firstName);
+// let lastName = prompt("Enter your last name");
+// console.log("name",lastName);
+// alert ("Hi, " + firstName + lastName);
+// //BÀI 8
+let myVariable = prompt("Enter side length of the square");
+ 
+ alart("The square erea is " + myVariable*2);
